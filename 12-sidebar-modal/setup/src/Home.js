c73aca3a2a81ece7c216311b9207af278e50dcba
Adriano Catena <adricatena@gmail.com>
2021-07-09 22:30:37 -0300

    fin proyecto 12 5.44.41

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,16 +1,21 @@
-import React, { useContext } from "react";
+// import React, { useContext } from "react"; // Usando context con custom hook ahorramos codigo
+// import { FaBars } from "react-icons/fa";
+// import { AppContext } from "./context";
+import React from "react";
 import { FaBars } from "react-icons/fa";
-import { AppContext } from "./context";
+import { useGlobalContext } from "./context";
 
 const Home = () => {
-  const data = useContext(AppContext);
-  console.log(data);
+  //const data = useContext(AppContext);
+  const { openSidebar, openModal } = useGlobalContext();
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars />
       </button>
-      <button className="btn">Show modal</button>
+      <button className="btn" onClick={openModal}>
+        Show modal
+      </button>
     </main>
   );
 };
--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -3,7 +3,42 @@ import React, { useState, useContext } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isModalOpen,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
-export { AppContext, AppProvider };
+//custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+//export { AppContext, AppProvider };
+
+export { AppProvider };
